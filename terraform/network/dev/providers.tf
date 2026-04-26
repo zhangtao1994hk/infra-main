e0c34c156ee1258c98632dabf7669ef0e6ec90d3
tao <zhangtao1994hk@gmail.com>
2026-04-27 03:13:59 +0800

    fix cloudflare provider source

--- terraform/network/dev/providers.tf
+++ terraform/network/dev/providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare" # 👈 关键：必须指定是 cloudflare 组织下的
+      version = "~> 4.0"                # 建议固定主版本号
+    }
+  }
+
   backend "gcs" {
     bucket = "zhangtao-tf-state"
     prefix = "terraform/network/dev" # 👈 必须与 compute 不同
