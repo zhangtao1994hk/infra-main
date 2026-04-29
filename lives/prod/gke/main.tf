8d7e9eb37fee34cf4960dc4ba5aa23f4bac6538e
tao <zhangtao1994hk@gmail.com>
2026-04-30 03:31:26 +0800

    add prod gke

---
+++ lives/prod/gke/main.tf
@@ -0,0 +1,25 @@
+data "terraform_remote_state" "network" {
+  backend = "gcs"
+
+  config = {
+    bucket = "zhangtao-tf-state"
+    prefix = "infra-main/lives/dev/network"
+  }
+}
+
+module "gke" {
+  source = "../../../modules/gcp-gke"
+
+  name       = "core"
+  env        = "prod"
+  project_id = "project-ef3e3c7b-875b-4ab7-a17"
+  region     = "us-central1"
+  
+  use_spot   = false
+
+  network    = data.terraform_remote_state.network.outputs.vpc_self_link
+  subnetwork = data.terraform_remote_state.network.outputs.subnet_self_links["subnet-a"]
+  
+  min_nodes = 1
+  max_nodes = 3
+}
